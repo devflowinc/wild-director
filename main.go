2ed1631a347b892b6d5d3ceabedba403f3dbfd74
Adhityaa Chandrasekar <adtac@subtrace.dev>
2024-12-10 17:15:24 -0500

    main.go: add http server

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/miekg/dns"
 )
 
 type server struct {
-	addr net.IP
 }
 
 func (s *server) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
@@ -17,21 +20,36 @@ func (s *server) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 		return
 	}
 
-	fmt.Printf("new DNS request: %q\n", r.Question[0].Name)
+	fmt.Printf("dns: new type %04x request: %q\n", r.Question[0].Qtype, r.Question[0].Name)
 
-	// TODO: make sure this is for the subdomain we care about
 	m := new(dns.Msg)
 	m.SetReply(r)
-	m.Authoritative = true
-	m.Answer = append(m.Answer, &dns.A{
-		Hdr: dns.RR_Header{
-			Name:   r.Question[0].Name,
-			Rrtype: dns.TypeA,
-			Class:  dns.ClassINET,
-			Ttl:    0,
-		},
-		A: s.addr,
-	})
+
+	switch r.Question[0].Qtype {
+	case dns.TypeTXT:
+		m.Answer = append(m.Answer, &dns.TXT{
+			Hdr: dns.RR_Header{
+				Name:   r.Question[0].Name,
+				Rrtype: dns.TypeTXT,
+				Class:  dns.ClassINET,
+				Ttl:    60,
+			},
+			Txt: []string{os.Getenv("WILD_TXT")},
+		})
+
+	case dns.TypeA:
+		// TODO: make sure this is for the subdomain we care about
+		m.Authoritative = true
+		m.Answer = append(m.Answer, &dns.A{
+			Hdr: dns.RR_Header{
+				Name:   r.Question[0].Name,
+				Rrtype: dns.TypeA,
+				Class:  dns.ClassINET,
+				Ttl:    60,
+			},
+			A: net.ParseIP(os.Getenv("WILD_A")),
+		})
+	}
 
 	if err := w.WriteMsg(m); err != nil {
 		panic(err)
@@ -39,14 +57,6 @@ func (s *server) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 }
 
 func main() {
-	s := &server{addr: net.ParseIP("5.78.103.252")}
-	if val := os.Getenv("WILD_ADDR"); val != "" {
-		s.addr = net.ParseIP(val)
-		if s.addr == nil {
-			panic(fmt.Sprintf("parse WILD_ADDR failed"))
-		}
-	}
-
 	pc, err := net.ListenPacket("udp", "0.0.0.0:53")
 	if err != nil {
 		panic(err)
@@ -55,7 +65,51 @@ func main() {
 
 	fmt.Printf("listening on 0.0.0.0:53...\n")
 
-	if err := dns.ActivateAndServe(nil, pc, s); err != nil {
-		panic(err)
+	go func() {
+		if err := dns.ActivateAndServe(nil, pc, new(server)); err != nil {
+			panic(err)
+		}
+	}()
+
+	cfg := make(map[string]string)
+	b, err := os.ReadFile("/etc/wild.json")
+	if err != nil {
+		panic(fmt.Errorf("read /etc/wild.json: %w", err))
+	}
+	if err := json.Unmarshal(b, &cfg); err != nil {
+		panic(fmt.Errorf("parse /etc/wild.json: %w", err))
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		uuid, ok := cfg[r.Host]
+		if !ok {
+			w.WriteHeader(404)
+			return
+		}
+
+		url := fmt.Sprintf("https://api.trieve.ai/public_page/%s", uuid)
+		fmt.Printf("http: remapping %q -> %q\n", r.URL.String(), url)
+
+		w2, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+
+		for key, vals := range w2.Header {
+			for _, val := range vals {
+				w.Header().Set(key, val)
+			}
+		}
+
+		w.WriteHeader(w2.StatusCode)
+
+		io.Copy(w, w2.Body)
+	})
+
+	for {
+		if err := http.ListenAndServeTLS("0.0.0.0:443", "/etc/letsencrypt/live/demo.trytrieve.com/fullchain.pem", "/etc/letsencrypt/live/demo.trytrieve.com/privkey.pem", nil); err != nil {
+			fmt.Printf("listen http failed: %v\n", err)
+			time.Sleep(5 * time.Second)
+		}
 	}
 }
